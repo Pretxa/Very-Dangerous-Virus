79d73b4a71af302335bf3dc392f014634f0d03cf
Pretza <69403099+Pretxa@users.noreply.github.com>
2021-09-08 23:01:41 +0200

    Add files via upload

---
+++ VeryDangerousVirusBeware.py
@@ -0,0 +1,15 @@
+import turtle #Dont be fooled by the name, this imports a very illeagel library that contains methods that can be used to  design the most elite and sophisticated malware. Even 'Lazarus' dosen't know about this one.
+
+turtle.speed(1000000000024) #This method dictates the level of encryption. (Only polymeric falcighol derivation is capable to decrpyt this)
+turtle.color("lime") #Again, don't be fooled by the name, "lime" is the code name for "Frustra quisquiliarum" only the top elitist hackers know what this means.
+
+#Anything beyond this line is way too sacred to be explained. If you wish to know more, the Marianas Web is the place to go. 
+
+turtle.bgcolor("black")
+
+b = 200
+
+while b > 0:
+	turtle.left(b)
+	turtle.forward(b*3)
+	b -= 1
